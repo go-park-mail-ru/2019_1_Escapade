e825e9ba55ee3c095c137d34e3071b2f6d691604
Bigyin <sergei.aparin1955@yandex.ru>
2019-03-08 22:54:13 +0300

    debug

--- internal/services/api/api.go
+++ internal/services/api/api.go
@@ -105,6 +105,7 @@ func (h *Handler) Register(rw http.ResponseWriter, r *http.Request) {
 	}
 	const place = "Register"
 	user, err := getUser(r)
+	fmt.Println(user.Email, user.Name, user.Email)
 	var sessionID string
 	sessionID, err = h.DB.Register(&user)
 
