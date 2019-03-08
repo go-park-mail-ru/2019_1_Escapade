116ea3b4d7435b0e05a19da309097f0cf2304dd9
Bigyin <sergei.aparin1955@yandex.ru>
2019-03-08 21:07:29 +0300

    add options method

--- main.go
+++ main.go
@@ -35,11 +35,11 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", mi.CORS(conf.Cors)(API.Ok))
-	r.HandleFunc("/register", mi.CORS(conf.Cors)(API.Register)).Methods("POST")
+	r.HandleFunc("/register", mi.CORS(conf.Cors)(API.Register)).Methods("POST", "OPTIONS")
 	r.HandleFunc("/delete", mi.CORS(conf.Cors)(API.DeleteAccount)).Methods("DELETE")
 	r.HandleFunc("/delete", mi.CORS(conf.Cors)(API.DeleteAccountOptions)).Methods("OPTIONS")
 
-	r.HandleFunc("/login", mi.CORS(conf.Cors)(API.Login)).Methods("POST")
+	r.HandleFunc("/login", mi.CORS(conf.Cors)(API.Login)).Methods("POST", "OPTIONS")
 	r.HandleFunc("/me", mi.CORS(conf.Cors)(API.Me)).Methods("GET")
 	r.HandleFunc("/{name}/games", mi.CORS(conf.Cors)(API.GetPlayerGames)).Methods("GET")
 	r.HandleFunc("/{name}/profile", mi.CORS(conf.Cors)(API.GetProfile)).Methods("GET")
