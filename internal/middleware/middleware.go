df53fb555a74374fc027f60a7c0596e089f57911
Artyom <artyomd2009@yandex.ru>
2019-04-15 16:58:11 +0300

    cors fix

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -11,17 +11,18 @@ import (
 	"strings"
 )
 
-type handleDecorator func(http.HandlerFunc) http.HandlerFunc
+// HandleDecorator middleware
+type HandleDecorator func(http.HandlerFunc) http.HandlerFunc
 
-func setCORS(rw http.ResponseWriter, cc config.CORSConfig) {
-	rw.Header().Set("Access-Control-Allow-Origin", strings.Join(cc.Origins, ", "))
+func setCORS(rw http.ResponseWriter, cc config.CORSConfig, name string) {
+	rw.Header().Set("Access-Control-Allow-Origin", name)
 	rw.Header().Set("Access-Control-Allow-Headers", strings.Join(cc.Headers, ", "))
 	rw.Header().Set("Access-Control-Allow-Credentials", cc.Credentials)
 	rw.Header().Set("Access-Control-Allow-Methods", strings.Join(cc.Methods, ", "))
 }
 
 // CORS Access-Control-Allow-Origin
-func CORS(cc config.CORSConfig, preCORS bool) handleDecorator {
+func CORS(cc config.CORSConfig, preCORS bool) HandleDecorator {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(rw http.ResponseWriter, r *http.Request) {
 			origin := r.Header.Get("Origin")
@@ -31,7 +32,7 @@ func CORS(cc config.CORSConfig, preCORS bool) handleDecorator {
 				rw.WriteHeader(http.StatusForbidden)
 				return
 			}
-			setCORS(rw, cc)
+			setCORS(rw, cc, origin)
 
 			if preCORS {
 				rw.WriteHeader(http.StatusOK)
@@ -43,8 +44,8 @@ func CORS(cc config.CORSConfig, preCORS bool) handleDecorator {
 	}
 }
 
-// Check cookie
-func Auth() handleDecorator {
+// Auth Check cookie exists
+func Auth() HandleDecorator {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(rw http.ResponseWriter, r *http.Request) {
 			cookie, err := r.Cookie(cookie.NameCookie)
@@ -60,7 +61,8 @@ func Auth() handleDecorator {
 	}
 }
 
-func Recover() handleDecorator {
+//Recover catch panic
+func Recover() HandleDecorator {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(rw http.ResponseWriter, r *http.Request) {
 			defer func() {
@@ -75,9 +77,9 @@ func Recover() handleDecorator {
 	}
 }
 
-// ApplyDecorators
+// ApplyMiddleware apply middleware
 func ApplyMiddleware(handler http.HandlerFunc,
-	decorators ...handleDecorator) http.HandlerFunc {
+	decorators ...HandleDecorator) http.HandlerFunc {
 	handler = Recover()(handler)
 	for _, m := range decorators {
 		handler = m(handler)
