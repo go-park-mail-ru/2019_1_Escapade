d87f98a1e05b9a9d049135a21c3535ad527ba679
Артём <artyomd2009@yandex.ru>
2019-11-03 12:15:25 +0300

    more metrics init functions

--- internal/services/game/metrics/metrics.go
+++ internal/services/game/metrics/metrics.go
@@ -51,150 +51,233 @@ func Init() {
 	// все числа в конфиг
 
 	// Lobby characteristics
-	FinishedRooms = prometheus.NewCounter(prometheus.CounterOpts{
-		Name:      "FinishedRooms",
-		Namespace: nLobby,
-		Subsystem: subsystem,
-		Help:      "Number of successfully completed games",
-	})
-	AbortedRooms = prometheus.NewCounter(prometheus.CounterOpts{
-		Name:      "AbortedRooms",
-		Namespace: nLobby,
-		Subsystem: subsystem,
-		Help:      "Number of aborted games",
-	})
-	ActiveRooms = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "ActiveRooms",
-		Namespace: nLobby,
-		Subsystem: subsystem,
-		Help:      "Number of playing rooms",
-	})
-	RecruitmentRooms = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "RecruitmentRooms",
-		Namespace: nLobby,
-		Subsystem: subsystem,
-		Help:      "Number of recruiting rooms",
-	})
-	LobbyMessages = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "Messages",
-		Namespace: nLobby,
-		Subsystem: subsystem,
-		Help:      "Number of sent messages",
-	})
+	FinishedRooms = initFinishedRooms(nLobby, subsystem)
+	AbortedRooms = initAbortedRooms(nLobby, subsystem)
+	ActiveRooms = initActiveRooms(nLobby, subsystem)
+	RecruitmentRooms = initRecruitmentRooms(nLobby, subsystem)
+	LobbyMessages = initLobbyMessages(nLobby, subsystem)
 
-	// Lobby characteristics
-	RoomsReconnections = prometheus.NewCounter(prometheus.CounterOpts{
-		Name:      "Reconnections",
-		Namespace: nAllRooms,
-		Subsystem: subsystem,
-		Help:      "Number of reconnections in game",
-	})
-
-	RoomsMessages = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "Messages",
-		Namespace: nAllRooms,
-		Subsystem: subsystem,
-		Help:      "Number of sent messages",
-	})
+	// All rooms characteristics
+	RoomsReconnections = initRoomsReconnections(nAllRooms, subsystem)
+	RoomsMessages = initRoomsMessages(nAllRooms, subsystem)
 
 	// Finished rooms characteristics
-	RoomPlayers = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:      "Players",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Buckets:   prometheus.ExponentialBuckets(2, 2, 7),
-		Help:      "Number of players who played the game",
-	}, []string{"room_type"})
-	RoomDifficult = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:      "Difficult",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Buckets:   []float64{0.2, 0.5, 0.9},
-		Help:      "Complexity of the game",
-	}, []string{"room_type"})
-	RoomSize = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:      "Size",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Buckets:   prometheus.ExponentialBuckets(100, 4, 5),
-		Help:      "Size of the field",
-	}, []string{"room_type"})
-	RoomTime = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:      "Time",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Buckets:   prometheus.ExponentialBuckets(1, 10, 5),
-		Help:      "The most time allotted for the game",
-	}, []string{"room_type"})
-	RoomOpenProcent = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:      "Procent",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Buckets:   []float64{0.3, 0.5, 0.8, 0.9, 1},
-		Help:      "The percentage opening of the field",
-	})
-	RoomMode = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name:      "Mode",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Help:      "Deathmatch or not",
-	}, []string{"room_type", "deathmatch"})
-	RoomAnonymous = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name:      "Anonymous",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Help:      "Anonymous disable[1]/anonymous enable(and they are in game)[2]//anonymous enable(but they are not in game)[3]",
-	}, []string{"room_type", "anonymous"})
-	RoomTimeSearchingPeople = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:      "TimeSearchingPeople",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Buckets:   prometheus.ExponentialBuckets(1, 10, 5),
-		Help:      "Time spent recruiting people",
-	}, []string{"room_type"})
-	RoomTimePlaying = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:      "TimePlaying",
-		Namespace: nFinishedGames,
-		Subsystem: subsystem,
-		Buckets:   prometheus.ExponentialBuckets(1, 10, 5),
-		Help:      "Time spent playing",
-	})
+	RoomPlayers = initRoomPlayers(nFinishedGames, subsystem)
+	RoomDifficult = initRoomDifficult(nFinishedGames, subsystem)
+	RoomSize = initRoomSize(nFinishedGames, subsystem)
+	RoomTime = initRoomTime(nFinishedGames, subsystem)
+	RoomOpenProcent = initRoomOpenProcent(nFinishedGames, subsystem)
+	RoomMode = initRoomMode(nFinishedGames, subsystem)
+	RoomAnonymous = initRoomAnonymous(nFinishedGames, subsystem)
+	RoomTimeSearchingPeople = initRoomTimeSearchingPeople(nFinishedGames, subsystem)
+	RoomTimePlaying = initRoomTimePlaying(nFinishedGames, subsystem)
 
 	// Users
-	Online = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "Online",
-		Namespace: nUsers,
-		Subsystem: subsystem,
-		Help:      "Users online at one moment",
-	})
-	AnonymousOnline = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "AnonymousOnline",
-		Namespace: nUsers,
-		Subsystem: subsystem,
-		Help:      "Anonymous users online at one moment",
-	})
-	Visits = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "Visits",
-		Namespace: nUsers,
-		Subsystem: subsystem,
-		Help:      "Number of visits",
-	})
-	InLobby = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "InLobby",
-		Namespace: nUsers,
-		Subsystem: subsystem,
-		Help:      "Users in lobby at one moment",
-	})
-	InGame = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:      "InGame",
-		Namespace: nUsers,
-		Subsystem: subsystem,
-		Help:      "Users in games at one moment",
-	})
+	Online = initOnline(nUsers, subsystem)
+	AnonymousOnline = initAnonymousOnline(nUsers, subsystem)
+	Visits = initVisits(nUsers, subsystem)
+	InLobby = initInLobby(nUsers, subsystem)
+	InGame = initInGame(nUsers, subsystem)
 
 	prometheus.MustRegister(FinishedRooms, AbortedRooms, ActiveRooms,
 		RoomPlayers, RoomDifficult, RoomSize, RoomTime, RoomOpenProcent,
 		RoomMode, RoomAnonymous, RoomTimeSearchingPeople, RoomTimePlaying,
 		Online, AnonymousOnline, Visits, InLobby, InGame, LobbyMessages,
 		RoomsMessages, RoomsReconnections)
+}
+
+func initFinishedRooms(nLobby, subsystem string) prometheus.Counter {
+
+	return prometheus.NewCounter(prometheus.CounterOpts{
+		Name:      "FinishedRooms",
+		Namespace: nLobby,
+		Subsystem: subsystem,
+		Help:      "Number of successfully completed games",
+	})
+}
+
+func initAbortedRooms(nLobby, subsystem string) prometheus.Counter {
+	return prometheus.NewCounter(prometheus.CounterOpts{
+		Name:      "AbortedRooms",
+		Namespace: nLobby,
+		Subsystem: subsystem,
+		Help:      "Number of aborted games",
+	})
+}
+
+func initActiveRooms(nLobby, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "ActiveRooms",
+		Namespace: nLobby,
+		Subsystem: subsystem,
+		Help:      "Number of playing rooms",
+	})
+}
+
+func initRecruitmentRooms(nLobby, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "RecruitmentRooms",
+		Namespace: nLobby,
+		Subsystem: subsystem,
+		Help:      "Number of recruiting rooms",
+	})
+}
+
+func initLobbyMessages(nLobby, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "Messages",
+		Namespace: nLobby,
+		Subsystem: subsystem,
+		Help:      "Number of sent messages",
+	})
+}
+
+func initRoomsReconnections(nAllRooms, subsystem string) prometheus.Counter {
+	return prometheus.NewCounter(prometheus.CounterOpts{
+		Name:      "Reconnections",
+		Namespace: nAllRooms,
+		Subsystem: subsystem,
+		Help:      "Number of reconnections in game",
+	})
+}
+
+func initRoomsMessages(nAllRooms, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "Messages",
+		Namespace: nAllRooms,
+		Subsystem: subsystem,
+		Help:      "Number of sent messages",
+	})
+}
+
+func initRoomPlayers(nFinishedGames, subsystem string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:      "Players",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Buckets:   prometheus.ExponentialBuckets(2, 2, 7),
+		Help:      "Number of players who played the game",
+	}, []string{"room_type"})
+}
+
+func initRoomDifficult(nFinishedGames, subsystem string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:      "Difficult",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Buckets:   []float64{0.2, 0.5, 0.9},
+		Help:      "Complexity of the game",
+	}, []string{"room_type"})
+}
+func initRoomSize(nFinishedGames, subsystem string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:      "Size",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Buckets:   prometheus.ExponentialBuckets(100, 4, 5),
+		Help:      "Size of the field",
+	}, []string{"room_type"})
+}
+
+func initRoomTime(nFinishedGames, subsystem string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:      "Time",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Buckets:   prometheus.ExponentialBuckets(1, 10, 5),
+		Help:      "The most time allotted for the game",
+	}, []string{"room_type"})
+}
+
+func initRoomOpenProcent(nFinishedGames, subsystem string) prometheus.Histogram {
+	return prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:      "Procent",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Buckets:   []float64{0.3, 0.5, 0.8, 0.9, 1},
+		Help:      "The percentage opening of the field",
+	})
+}
+
+func initRoomMode(nFinishedGames, subsystem string) *prometheus.GaugeVec {
+	return prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name:      "Mode",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Help:      "Deathmatch or not",
+	}, []string{"room_type", "deathmatch"})
+}
+
+func initRoomAnonymous(nFinishedGames, subsystem string) *prometheus.GaugeVec {
+	return prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name:      "Anonymous",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Help:      "Anonymous disable[1]/anonymous enable(and they are in game)[2]//anonymous enable(but they are not in game)[3]",
+	}, []string{"room_type", "anonymous"})
+}
+
+func initRoomTimeSearchingPeople(nFinishedGames, subsystem string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:      "TimeSearchingPeople",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Buckets:   prometheus.ExponentialBuckets(1, 10, 5),
+		Help:      "Time spent recruiting people",
+	}, []string{"room_type"})
+}
+
+func initRoomTimePlaying(nFinishedGames, subsystem string) prometheus.Histogram {
+	return prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:      "TimePlaying",
+		Namespace: nFinishedGames,
+		Subsystem: subsystem,
+		Buckets:   prometheus.ExponentialBuckets(1, 10, 5),
+		Help:      "Time spent playing",
+	})
+}
+
+func initOnline(nUsers, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "Online",
+		Namespace: nUsers,
+		Subsystem: subsystem,
+		Help:      "Users online at one moment",
+	})
+}
+
+func initAnonymousOnline(nUsers, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "AnonymousOnline",
+		Namespace: nUsers,
+		Subsystem: subsystem,
+		Help:      "Anonymous users online at one moment",
+	})
+}
+
+func initVisits(nUsers, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "Visits",
+		Namespace: nUsers,
+		Subsystem: subsystem,
+		Help:      "Number of visits",
+	})
+}
+
+func initInLobby(nUsers, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "InLobby",
+		Namespace: nUsers,
+		Subsystem: subsystem,
+		Help:      "Users in lobby at one moment",
+	})
+}
+
+func initInGame(nUsers, subsystem string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "InGame",
+		Namespace: nUsers,
+		Subsystem: subsystem,
+		Help:      "Users in games at one moment",
+	})
 }
