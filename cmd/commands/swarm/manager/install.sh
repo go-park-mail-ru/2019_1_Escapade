b17634e8668b7b76282c51466457d3ca3283b62b
Artyom <artyomd2009@yandex.ru>
2019-11-30 11:29:07 +0000

    reduce docker chat image from 980MB to 17.2MB

--- cmd/commands/swarm/manager/install.sh
+++ cmd/commands/swarm/manager/install.sh
@@ -39,3 +39,17 @@ else
     wget https://github.com/wagoodman/dive/releases/download/v0.9.1/dive_0.9.1_linux_amd64.deb
     sudo apt install ./dive_0.9.1_linux_amd64.deb
 fi
+
+echo "\n4. Install golang"
+place=$(whereis go | grep bin)
+if [ "$?" = "0" ]; then
+    echo "✔ " $place
+else
+    VERSION=1.13.4
+    OS=linux
+    ARCH=amd64
+    sudo curl -O https://storage.googleapis.com/golang/go$VERSION.$OS-$ARCH.tar.gz
+    tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+    rm go$VERSION.$OS-$ARCH.tar.gz
+    export PATH=$PATH:/usr/local/go/bin
+fi
